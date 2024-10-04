6fa1336cc00ed8b959bbac8a7cca07fb7a6a32f9
DmHirushan <dimuthsamaranayaka756@gmail.com>
2024-10-04 12:57:45 +0530

    dependency injected to CustomerServiceImpl

--- src/main/java/lk/ijse/gdse/aad/possystemusingspring/service/CustomerServiceImpl.java
+++ src/main/java/lk/ijse/gdse/aad/possystemusingspring/service/CustomerServiceImpl.java
@@ -1,9 +1,23 @@
 package lk.ijse.gdse.aad.possystemusingspring.service;
 
+import jakarta.transaction.Transactional;
+import lk.ijse.gdse.aad.possystemusingspring.dao.CustomerDao;
 import lk.ijse.gdse.aad.possystemusingspring.dto.CustomerDto;
+import lk.ijse.gdse.aad.possystemusingspring.entity.Customer;
+import lk.ijse.gdse.aad.possystemusingspring.util.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
+@Service
+@Transactional
 public class CustomerServiceImpl implements CustomerService{
+
+    @Autowired
+    private CustomerDao customerDao;
+    @Autowired
+    private Mapping mapping;
+
     @Override
     public void saveCustomer(CustomerDto customerDto) {
 

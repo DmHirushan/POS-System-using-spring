a8904d7278060f5085cda46a8318b4c7564b7043
DmHirushan <dimuthsamaranayaka756@gmail.com>
2024-10-04 10:34:54 +0530

    basic dependencies added

--- src/main/java/lk/ijse/gdse/aad/possystemusingspring/AppInitializer.java
+++ src/main/java/lk/ijse/gdse/aad/possystemusingspring/AppInitializer.java
@@ -1,7 +1,21 @@
 package lk.ijse.gdse.aad.possystemusingspring;
 
-public class AppInitializer {
-    public static void main(String[] args) {
-        System.out.println("Bdu Wda....");
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
     }
 }

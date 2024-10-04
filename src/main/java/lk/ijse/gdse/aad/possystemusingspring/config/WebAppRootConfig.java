098ee70a92129f132003e51ac73c6ca0a758ac39
DmHirushan <dimuthsamaranayaka756@gmail.com>
2024-10-04 10:50:13 +0530

    annotations added to configuration classes

--- src/main/java/lk/ijse/gdse/aad/possystemusingspring/config/WebAppRootConfig.java
+++ src/main/java/lk/ijse/gdse/aad/possystemusingspring/config/WebAppRootConfig.java
@@ -1,4 +1,13 @@
 package lk.ijse.gdse.aad.possystemusingspring.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@ComponentScan(basePackages = "lk.ijse.gdse.aad.possystemusingspring")
+@EnableJpaRepositories(basePackages = "lk.ijse.gdse.aad.possystemusingspring")
+@EnableTransactionManagement
 public class WebAppRootConfig {
 }

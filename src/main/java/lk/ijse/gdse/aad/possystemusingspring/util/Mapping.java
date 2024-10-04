83550386d56aa6ca6f8fb18d2690fd21b2196f00
DmHirushan <dimuthsamaranayaka756@gmail.com>
2024-10-04 20:03:06 +0530

    convertToDtos function implemented

--- src/main/java/lk/ijse/gdse/aad/possystemusingspring/util/Mapping.java
+++ src/main/java/lk/ijse/gdse/aad/possystemusingspring/util/Mapping.java
@@ -4,9 +4,11 @@ import lk.ijse.gdse.aad.possystemusingspring.customObj.CustomerResponse;
 import lk.ijse.gdse.aad.possystemusingspring.dto.CustomerDto;
 import lk.ijse.gdse.aad.possystemusingspring.entity.Customer;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Optional;
 
 @Component
@@ -22,4 +24,7 @@ public class Mapping {
         return modelMapper.map(customer, CustomerDto.class);
     }
 
+    public List<CustomerDto> convertToDtos(List<Customer> customerList){
+        return modelMapper.map(customerList, new TypeToken<List<CustomerDto>>(){}.getType());
+    }
 }

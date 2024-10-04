bc995ad3ab2d4f3d09d0fbb1b2fda67c06fc31f6
DmHirushan <dimuthsamaranayaka756@gmail.com>
2024-10-04 15:56:06 +0530

    validation added in saveCustomer on CustomerServiceImpl

--- src/main/java/lk/ijse/gdse/aad/possystemusingspring/service/CustomerServiceImpl.java
+++ src/main/java/lk/ijse/gdse/aad/possystemusingspring/service/CustomerServiceImpl.java
@@ -4,6 +4,7 @@ import jakarta.transaction.Transactional;
 import lk.ijse.gdse.aad.possystemusingspring.dao.CustomerDao;
 import lk.ijse.gdse.aad.possystemusingspring.dto.CustomerDto;
 import lk.ijse.gdse.aad.possystemusingspring.entity.Customer;
+import lk.ijse.gdse.aad.possystemusingspring.exception.DataPersistFailedException;
 import lk.ijse.gdse.aad.possystemusingspring.util.Mapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +21,14 @@ public class CustomerServiceImpl implements CustomerService{
 
     @Override
     public void saveCustomer(CustomerDto customerDto) {
-        customerDao.save(mapping.convertToEntity(customerDto));
+        if (customerDao.existsById(customerDto.getCustomerId())) {
+            throw new DataPersistFailedException("This customer ID already exists!");
+        }else {
+            Customer savedCustomer = customerDao.save(mapping.convertToEntity(customerDto));
+            if (savedCustomer == null && savedCustomer.getCustomerId() == null) {
+                throw new DataPersistFailedException("Can't save the customer!");
+            }
+        }
+
     }
 }

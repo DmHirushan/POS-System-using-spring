3c2a2fb74d61a477ef48ecfe74ee162db11b847e
DmHirushan <dimuthsamaranayaka756@gmail.com>
2024-10-05 18:54:06 +0530

    saveItem done in Controller

--- src/main/java/lk/ijse/gdse/aad/possystemusingspring/controller/ItemController.java
+++ src/main/java/lk/ijse/gdse/aad/possystemusingspring/controller/ItemController.java
@@ -1,13 +1,34 @@
 package lk.ijse.gdse.aad.possystemusingspring.controller;
 
+import lk.ijse.gdse.aad.possystemusingspring.dto.ItemDto;
+import lk.ijse.gdse.aad.possystemusingspring.exception.DataPersistFailedException;
+import lk.ijse.gdse.aad.possystemusingspring.service.ItemService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/item")
 @RequiredArgsConstructor
 public class ItemController {
+    @Autowired
+    private ItemService itemService;
 
+    @PostMapping
+    public ResponseEntity<Void> saveItem(@RequestBody ItemDto itemDto){
+        if (itemDto == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } else {
+            try {
+                itemService.saveItem(itemDto);
+                return new ResponseEntity<>(HttpStatus.CREATED);
+            } catch (DataPersistFailedException e) {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            } catch (Exception e) {
+                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
+    }
 }

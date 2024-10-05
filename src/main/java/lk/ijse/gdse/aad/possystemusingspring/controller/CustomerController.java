633dc5318c319c6a07bb23f8610659509e203246
DmHirushan <dimuthsamaranayaka756@gmail.com>
2024-10-05 16:47:46 +0530

    allow cross origin in CustomerController

--- src/main/java/lk/ijse/gdse/aad/possystemusingspring/controller/CustomerController.java
+++ src/main/java/lk/ijse/gdse/aad/possystemusingspring/controller/CustomerController.java
@@ -17,6 +17,7 @@ import java.util.List;
 @RestController
 @RequestMapping("api/v1/customer")
 @RequiredArgsConstructor
+@CrossOrigin
 public class CustomerController {
     @Autowired
     private final CustomerService customerService;

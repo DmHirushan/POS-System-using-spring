6d2b252163b5d541ec9a7ea8923342ea9d0272e3
DmHirushan <dimuthsamaranayaka756@gmail.com>
2024-10-05 18:45:38 +0530

    ItemController created

---
+++ src/main/java/lk/ijse/gdse/aad/possystemusingspring/controller/ItemController.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse.aad.possystemusingspring.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/item")
+@RequiredArgsConstructor
+public class ItemController {
+
+}

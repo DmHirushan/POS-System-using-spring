b60f306ba47a9146e24a7e6c4971231e554b73ab
DmHirushan <dimuthsamaranayaka756@gmail.com>
2024-10-07 17:12:02 +0530

    CrossOrigin annotation added

--- src/main/java/lk/ijse/gdse/aad/possystemusingspring/controller/ItemController.java
+++ src/main/java/lk/ijse/gdse/aad/possystemusingspring/controller/ItemController.java
@@ -16,6 +16,7 @@ import java.util.List;
 @RestController
 @RequestMapping("api/v1/item")
 @RequiredArgsConstructor
+@CrossOrigin
 public class ItemController {
     @Autowired
     private ItemService itemService;

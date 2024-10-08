356799bd6ef85369043888e9a4518393dfcb36de
DmHirushan <dimuthsamaranayaka756@gmail.com>
2024-10-08 18:33:54 +0530

    PlaceOrderFormController converted as rest controller

--- src/main/java/lk/ijse/gdse/aad/possystemusingspring/controller/PlaceOrderFormController.java
+++ src/main/java/lk/ijse/gdse/aad/possystemusingspring/controller/PlaceOrderFormController.java
@@ -1,5 +1,14 @@
 package lk.ijse.gdse.aad.possystemusingspring.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("api/v1/placeorder")
 public class PlaceOrderFormController {
+    @GetMapping
+    public String healthCheck(){
+        return "Place Order up and running!";
+    }
 }

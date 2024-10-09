9bcae21fd763c75c0bc95bc5b7435b683d59891a
DmHirushan <dimuthsamaranayaka756@gmail.com>
2024-10-09 19:37:42 +0530

    logs added

--- src/main/java/lk/ijse/gdse/aad/possystemusingspring/controller/PlaceOrderFormController.java
+++ src/main/java/lk/ijse/gdse/aad/possystemusingspring/controller/PlaceOrderFormController.java
@@ -3,7 +3,10 @@ package lk.ijse.gdse.aad.possystemusingspring.controller;
 import lk.ijse.gdse.aad.possystemusingspring.dto.OrderDetailDto;
 import lk.ijse.gdse.aad.possystemusingspring.dto.OrderDto;
 
+import lk.ijse.gdse.aad.possystemusingspring.exception.DataPersistFailedException;
 import lk.ijse.gdse.aad.possystemusingspring.service.OrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("api/v1/placeorder")
 public class PlaceOrderFormController {
+    private static final Logger logger = LoggerFactory.getLogger(PlaceOrderFormController.class);
     @Autowired
     private OrderService orderService;
 
@@ -20,8 +24,25 @@ public class PlaceOrderFormController {
 
     @PostMapping(value = "order")
     public ResponseEntity<Void> saveOrder(@RequestBody OrderDto orderDto){
-        orderService.saveOrder(orderDto);
-        return new ResponseEntity<>(HttpStatus.CREATED);
+        logger.info("Request to save order: {}", orderDto);
+        if (orderDto == null) {
+            logger.warn("Received null orderDto for saving: {}", orderDto);
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }else {
+            try{
+                orderService.saveOrder(orderDto);
+                logger.info("Successfully saved order: {}", orderDto);
+                return new ResponseEntity<>(HttpStatus.CREATED);
+            } catch (DataPersistFailedException e) {
+                logger.error("Failed to persist order data: {}", orderDto, e);
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            } catch (Exception e) {
+                logger.error("Unexpected error while saving order: {}", orderDto, e);
+                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+
+        }
+
     }
 
 //    @PostMapping(value = "order_detail")

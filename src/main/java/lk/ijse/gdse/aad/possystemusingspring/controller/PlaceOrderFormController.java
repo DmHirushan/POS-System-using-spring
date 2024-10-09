61b2f157745144e117c6902f8ce98bb77c17ef40
DmHirushan <dimuthsamaranayaka756@gmail.com>
2024-10-09 12:54:18 +0530

    saveOrder done in PlaceOrderFormController

--- src/main/java/lk/ijse/gdse/aad/possystemusingspring/controller/PlaceOrderFormController.java
+++ src/main/java/lk/ijse/gdse/aad/possystemusingspring/controller/PlaceOrderFormController.java
@@ -1,14 +1,21 @@
 package lk.ijse.gdse.aad.possystemusingspring.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lk.ijse.gdse.aad.possystemusingspring.dto.OrderDto;
+import lk.ijse.gdse.aad.possystemusingspring.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/placeorder")
 public class PlaceOrderFormController {
-    @GetMapping
-    public String healthCheck(){
-        return "Place Order up and running!";
+    @Autowired
+    private OrderService orderService;
+
+    @PostMapping
+    public ResponseEntity<Void> saveOrder(@RequestBody OrderDto orderDto){
+        orderService.saveOrder(orderDto);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }

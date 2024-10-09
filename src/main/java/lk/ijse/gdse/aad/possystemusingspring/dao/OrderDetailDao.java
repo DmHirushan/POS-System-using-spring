a08914d51ad6e5285dfac2bb6057d64b2a7651bc
DmHirushan <dimuthsamaranayaka756@gmail.com>
2024-10-09 14:28:30 +0530

    OrderDetailDao created

---
+++ src/main/java/lk/ijse/gdse/aad/possystemusingspring/dao/OrderDetailDao.java
@@ -0,0 +1,9 @@
+package lk.ijse.gdse.aad.possystemusingspring.dao;
+
+import lk.ijse.gdse.aad.possystemusingspring.entity.OrderDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderDetailDao extends JpaRepository <OrderDetail, String> {
+}

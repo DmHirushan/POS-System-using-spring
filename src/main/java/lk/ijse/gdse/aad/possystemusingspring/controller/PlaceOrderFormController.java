690d9db81fa7bbd0d0ede07263b8f180102a23f3
DmHirushan <dimuthsamaranayaka756@gmail.com>
2024-10-09 14:49:18 +0530

    error resolved by commenting relative changess

--- src/main/java/lk/ijse/gdse/aad/possystemusingspring/controller/PlaceOrderFormController.java
+++ src/main/java/lk/ijse/gdse/aad/possystemusingspring/controller/PlaceOrderFormController.java
@@ -1,6 +1,8 @@
 package lk.ijse.gdse.aad.possystemusingspring.controller;
 
+import lk.ijse.gdse.aad.possystemusingspring.dto.OrderDetailDto;
 import lk.ijse.gdse.aad.possystemusingspring.dto.OrderDto;
+
 import lk.ijse.gdse.aad.possystemusingspring.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,9 +15,18 @@ public class PlaceOrderFormController {
     @Autowired
     private OrderService orderService;
 
-    @PostMapping
+//    @Autowired
+//    private OrderDetailService orderDetailService;
+
+    @PostMapping(value = "order")
     public ResponseEntity<Void> saveOrder(@RequestBody OrderDto orderDto){
         orderService.saveOrder(orderDto);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
+
+//    @PostMapping(value = "order_detail")
+//    public ResponseEntity<Void> saveOrderDetail(@RequestBody OrderDetailDto orderDetailDto){
+//        orderDetailService.saveOrderDetails(orderDetailDto);
+//        return new ResponseEntity<>(HttpStatus.CREATED);
+//    }
 }
--- src/main/java/lk/ijse/gdse/aad/possystemusingspring/dao/OrderDetailDao.java
+++ src/main/java/lk/ijse/gdse/aad/possystemusingspring/dao/OrderDetailDao.java
@@ -1,9 +1,11 @@
-package lk.ijse.gdse.aad.possystemusingspring.dao;
-
-import lk.ijse.gdse.aad.possystemusingspring.entity.OrderDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderDetailDao extends JpaRepository <OrderDetail, String> {
-}
+//package lk.ijse.gdse.aad.possystemusingspring.dao;
+//
+//import lk.ijse.gdse.aad.possystemusingspring.dto.OrderDetailDto;
+//import lk.ijse.gdse.aad.possystemusingspring.entity.Order;
+//import lk.ijse.gdse.aad.possystemusingspring.entity.OrderDetail;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface OrderDetailDao extends JpaRepository <OrderDetailDto, Object> {
+//}

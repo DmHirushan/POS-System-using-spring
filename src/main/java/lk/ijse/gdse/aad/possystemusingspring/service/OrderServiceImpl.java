0bd74ab7e393c43b000a22d50343b3c5899b4651
DmHirushan <dimuthsamaranayaka756@gmail.com>
2024-10-09 12:16:32 +0530

    OrderService  | OrderServiceImpl created

---
+++ src/main/java/lk/ijse/gdse/aad/possystemusingspring/service/OrderServiceImpl.java
@@ -0,0 +1,10 @@
+package lk.ijse.gdse.aad.possystemusingspring.service;
+
+import lk.ijse.gdse.aad.possystemusingspring.dto.OrderDto;
+
+public class OrderServiceImpl implements OrderService {
+    @Override
+    public void saveOrder(OrderDto orderDto) {
+
+    }
+}
